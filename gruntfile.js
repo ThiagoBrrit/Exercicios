f45b3c9f889fe1a73696651d86cc7b6395916752
Thiago Brito <deoliveira.tbrito@gmail.com>
2023-07-11 19:03:12 -0300

    commit inicial

---
+++ gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
+            }
+        },
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'CSS_DEV',
+                            replacement: './styles/main.css'
+                        },
+                        {
+                            match: 'JS_DEV',
+                            replacement: '..src/scripts/main.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            uglify: {
+                target: {
+                    files: {
+                        'dist/scripts/main.min.js': 'src/scripts/main.js'
+                    }
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production','replace:dist', 'uglify']);
+}
