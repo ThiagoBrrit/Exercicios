4ffc6a7c2d8019444fcd1d3cc2506fd1f990c338
Thiago Brito <deoliveira.tbrito@gmail.com>
2023-08-21 18:33:44 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -4,37 +4,37 @@ import Cabecalho from './components/Cabecalho.vue';
 import Formulario from './components/Formulario.vue';
 import Resultado from './components/Resultado.vue';
 
-const calculadora = reactive({
-  filtro: 'dividindo',
-  A: '',
-  B: '',
-  resultado: '',
-})
+// const calculadora = reactive({
+//   filtro: 'dividindo',
+//   A: '',
+//   B: '',
+//   resultado: '',
+// })
 
-const somando = () => {
-  const Rsomando = calculadora.A + calculadora.B;
-  calculadora.resultado = Rsomando;
-  return calculadora.resultado
+// const somando = () => {
+//   const Rsomando = calculadora.A + calculadora.B;
+//   calculadora.resultado = Rsomando;
+//   return calculadora.resultado
 
-}
+// }
 
-const subtraindo = () => {
-  const Rsubtraindo = calculadora.A - calculadora.B;
-  calculadora.resultado = Rsubtraindo;
-  return calculadora.resultado
-}
+// const subtraindo = () => {
+//   const Rsubtraindo = calculadora.A - calculadora.B;
+//   calculadora.resultado = Rsubtraindo;
+//   return calculadora.resultado
+// }
 
-const multiplicando = () => {
-  const Rmultiplicando = calculadora.A * calculadora.B;
-  calculadora.resultado = Rmultiplicando;
-  return calculadora.resultado
-}
+// const multiplicando = () => {
+//   const Rmultiplicando = calculadora.A * calculadora.B;
+//   calculadora.resultado = Rmultiplicando;
+//   return calculadora.resultado
+// }
 
-const dividindo = () => {
-  const Rdividindo= calculadora.A / calculadora.B;
-  calculadora.resultado = Rdividindo;
-  return calculadora.resultado
-}
+// const dividindo = () => {
+//   const Rdividindo= calculadora.A / calculadora.B;
+//   calculadora.resultado = Rdividindo;
+//   return calculadora.resultado
+// }
 
 
 </script>
